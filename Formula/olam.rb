b0e5af067b02dee80cc619f537cbf043735037e5
Aboobacker MK <aboobackervyd@gmail.com>
2022-12-29 14:27:49 +0530

    Add olam formula

---
+++ Formula/olam.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class Olam < Formula
+  desc 'English Malayalam Dictionary'
+  homepage 'http://github.com/tachyons/olam'
+  head 'https://github.com/tachyons/olam.git'
+
+#   depends_on 'qmake' => :build
+  depends_on 'qt@5'
+
+  def install
+    mkdir 'build' do
+      system 'qmake'
+      system 'make'
+      prefix.install 'build/release/olam.app'
+      (bin / 'olam').write("#! /bin/sh\n#{prefix}/olam.app/Contents/MacOS/olam \"$@\"\n")
+    end
+  end
+
+  test do
+    system 'olam', '-h'
+  end
+end
