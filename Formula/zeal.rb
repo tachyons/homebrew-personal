856b233da648d973b437d11923548ed29f151461
Aboobacker MK <aboobackervyd@gmail.com>
2022-12-29 13:36:33 +0530

    Add zeal brew formula

---
+++ Formula/zeal.rb
@@ -0,0 +1,25 @@
+# From https://github.com/markwu/homebrew-personal/blob/master/Formula/zeal.rb
+require 'formula'
+
+class Zeal < Formula
+  desc 'Zeal is a simple offline documentation browser inspired by Dash.'
+  homepage 'http://zealdocs.org/'
+  head 'https://github.com/zealdocs/zeal.git'
+
+  depends_on 'cmake' => :build
+  depends_on 'qt@5'
+  depends_on 'libarchive'
+
+  def install
+    mkdir 'build' do
+      system 'cmake', '..', *std_cmake_args
+      system 'make'
+      prefix.install 'bin/Zeal.app'
+      (bin / 'zeal').write("#! /bin/sh\n#{prefix}/Zeal.app/Contents/MacOS/Zeal \"$@\"\n")
+    end
+  end
+
+  test do
+    system 'zeal', '-h'
+  end
+end
